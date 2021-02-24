d8c0d2a321c3662cfcc923d3927b1dd41affac5e
con0fav <connor.favero@gmail.com>
2021-02-24 01:52:21 -0800

    building ajax call, append functions

--- dashCode.js
+++ dashCode.js
@@ -1,71 +1,45 @@
-/**
- * pulls information from the form and build the query URL
- * @returns {string} URL for NYT API based on form inputs
- */
-function buildQueryURL() {
-    // queryURL is the url we'll use to query the API
-    var queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=6e5a87761f0106c2004ba7c2b661c085";
-  
-    // Grab text the user typed into the search input, add to the queryParams object
-    queryParams.q = $("#search-term")
-      .val()
-      .trim();
+// // WeatherAPIKey API AJAX CALL
+// function weatherAPI (city) {
+//   // var cityEl = city
+//   // cityEl = cityEl.replace(" " , "_");
+//   // console.log(cityEl);
+//   var WeatherAPIKey = "";
+//   var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + WeatherAPIKey;
+//   console.log(weatherURL);
+   
+//   $.ajax({ 
+//     url: weatherURL, 
+//     method: "GET" 
+//   }).then(function(response) {
+//     console.log(response)
+//     var tempF = (response.main.temp);          
+//     $("#weatherData").text("Temperature: " + tempF.toFixed(0)+"°");  
+//     var imageURL = response.weather[0].icon;
+//     var iconImg = $("<img>").attr("src","http://openweathermap.org/img/wn/"+imageURL+"@2x.png");
+//           console.log(imageURL)
+//     $("#weatherData").append(iconImg) 
+//     $("#citySearchBtn").on("click", function(event) {
+//       event.preventDefault();  
+//     });    
+//   });
+// };
+
+var search = document.querySelector();
+var result = document.querySelector();
+var fiveDay = document.querySelector();
 
 
+var APIKey = "";
+//API KEY GOES ABOVE
 
-}
-
-
-
-
+var openwURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Function to empty out the articles
-function clear() {
-  $("#article-section").empty();
-}
-
-// CLICK HANDLERS
-// ==========================================================
-
-// .on("click") function associated with the Search Button
-$("#run-search").on("click", function(event) {
-  // This line allows us to take advantage of the HTML "submit" property
-  // This way we can hit enter on the keyboard and it registers the search
-  // (in addition to clicks). Prevents the page from reloading on form submit.
-  event.preventDefault();
-
-  // Empty the region associated with the articles
-  clear();
-
-  // Build the query URL for the ajax request to the NYT API
-  var queryURL = buildQueryURL();
-
-  // Make the AJAX request to the API - GETs the JSON data at the queryURL.
-  // The data then gets passed as an argument to the updatePage function
-  $.ajax({
+$.ajax({
     url: queryURL,
     method: "GET"
-  }).then(updatePage);
+})
+.then(function(res) {
+    console.log(res);
+    console.log(queryURL);
 });
-
-//  .on("click") function associated with the clear button
-$("#clear-all").on("click", clear);
