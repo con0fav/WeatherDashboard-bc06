0734864ff8162e593dab0f84eb15c3f39dbfc7b0
con0fav <connor.favero@gmail.com>
2021-03-09 21:33:54 -0800

    restructuring dashCode, adding variables

--- dashCode.js
+++ dashCode.js
@@ -1,46 +1,51 @@
-// // WeatherAPIKey API AJAX CALL
-// function weatherAPI (city) {
-//   // var cityEl = city
-//   // cityEl = cityEl.replace(" " , "_");
-//   // console.log(cityEl);
-//   var WeatherAPIKey = "";
-//   var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + WeatherAPIKey;
-//   console.log(weatherURL);
-   
-//   $.ajax({ 
-//     url: weatherURL, 
-//     method: "GET" 
-//   }).then(function(response) {
-//     console.log(response)
-//     var tempF = (response.main.temp);          
-//     $("#weatherData").text("Temperature: " + tempF.toFixed(0)+"°");  
-//     var imageURL = response.weather[0].icon;
-//     var iconImg = $("<img>").attr("src","http://openweathermap.org/img/wn/"+imageURL+"@2x.png");
-//           console.log(imageURL)
-//     $("#weatherData").append(iconImg) 
-//     $("#citySearchBtn").on("click", function(event) {
-//       event.preventDefault();  
-//     });    
-//   });
-// };
+var city="";
 
-var search = document.querySelector();
-var result = document.querySelector();
-var fiveDay = document.querySelector();
+var searchCity = $("#searchCity");
+var searchButton = $("#searchButton");
+var clearButton = $("#clear");
+var searchedCity = $("#searchedCity");
+var cityTemp = $("#temperature");
+var cityHumidty= $("#humidity");
+var windSpeed=$("#wind-speed");
+var uvindex= $("#uv-index");
 
-
-var APIKey = "";
-//API KEY GOES ABOVE
-//f0b02fab26617617b2432ba827c42fc0
+var APIKey = "f0b02fab26617617b2432ba827c42fc0";
 
 var openwURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
 
+//when search is clicked
+function searchBar() {
+    makeRequest(search);
+}
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-})
-.then(function(res) {
-    console.log(res);
-    console.log(queryURL);
-});
+//then get val from search input
+
+function makeRequest (search) {
+
+    //build url for first API req
+    //make req to URL with jquery ajax call
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    .then(function(res) {
+        console.log(res);
+        console.log(queryURL);
+    });
+    
+    //start render data 
+    //get lat and long out of response object
+
+    //make oneCall request and pass in lat, long
+    //
+}
+
+//openweather onecall request
+function oneCall(lat, lng) {
+    
+    $.ajax(queryURL).then(function(res){
+        //finish rendering data
+    })
+
+}
