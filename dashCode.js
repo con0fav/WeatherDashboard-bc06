b43d03f776ec1b78243d87051e0e8cf60e08cae2
con0fav <connor.favero@gmail.com>
2021-03-10 19:41:25 -0800

    rewriting call, added clear button function

--- dashCode.js
+++ dashCode.js
@@ -12,28 +12,37 @@ var cityArray = [];
 
 var APIKey = "f0b02fab26617617b2432ba827c42fc0";
 
-function find(c){
-    for (var i=0; i<cityArray.length; i++){
-        if(c.toUpperCase()===cityArray[i]){
-            return -1;
-        }
-    }
-    return 1;
-}
+// //when search is clicked
+// function searchBar(event) {
+//     event.preventDefault();
+//     if (searchCity.val().trim()!=="") {
+//         city=searchCity.val().trim();
+//         getWeather(city);
+//     }
+// }
 
-//when search is clicked
-function searchBar(event) {
-    event.preventDefault();
-    if (searchCity.val().trim()!=="") {
-        city=searchCity.val().trim();
-        getWeather(city);
-    }
-    // makeRequest(search);
-}
+// function getWeather(city) {
+    
+//     //build url for first API req
+//     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
+//     //make req to URL with jquery ajax call
 
-//then get val from search input
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     })
+//     .then(function(res) {
 
-function getWeather(city) {
+//     });
+    
+//     //start render data 
+//     //get lat and long out of response object
+// }
+
+// $("searchButton").on("click", getWeather);
+
+
+$("#searchButton").on("click", function() {
     
     //build url for first API req
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
@@ -44,27 +53,26 @@ function getWeather(city) {
         method: "GET"
     })
     .then(function(res) {
+        console.log(queryURL);
+
         console.log(res);
 
-        var icon = res.weather[0].icon;
-        var iconCall = "https://openweathermap.org/img/wn/"+ icon +"@2x.png";
+        var results = res.data;
 
-        var date = new Date(res.dt*1000).toLocaleDateString();
-
-        $(currentCity).html(res.name + "("+date+")" + "<img src="+iconCall+">")
-
-        var temp = res.main.temp;
-        $(cityTemp).html(temp+"&#8457");
-
-        $(cityHumidty).html(res.main.humidity+"%");
-
-        var ws = res.wind.speed;
-        $(cityWind).html(ws);
+        //elements for:
+        //city name
+        //date
+        //temp
+        //humidity
+        //wind speed
+        //uv index
 
     });
-    
-    //start render data 
-    //get lat and long out of response object
+
+})
+
+function clear() {
+    $("#searchCity").empty();
 }
 
-$("searchButton").on("click", getWeather);
+$("#clearButton").on("click", clear);
