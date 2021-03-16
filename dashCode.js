63ed8fc7b2e5f01de9c04907901a6fbfb6e73277
con0fav <connor.favero@gmail.com>
2021-03-15 20:25:08 -0700

    built date function for cards

--- dashCode.js
+++ dashCode.js
@@ -1,4 +1,4 @@
-var city="";
+var city = "";
 
 var searchCity = $("#searchCity");
 var searchButton = $("#searchButton");
@@ -15,66 +15,75 @@ var cityArray = [];
 var APIKey = "f0b02fab26617617b2432ba827c42fc0";
 
 function getWeather(city) {
-    
+
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
 
     $.ajax({
         url: queryURL,
         method: "GET"
     })
-    .then(function(res) {
-        console.log(res)
+        .then(function (res) {
+            console.log(res)
 
-        cityTemp.text(res.main.temp);
-        cityHumidty.text(res.main.humidity);
-        cityWind.text(res.wind.speed);
+            cityTemp.text(res.main.temp);
+            cityHumidty.text(res.main.humidity);
+            cityWind.text(res.wind.speed);
 
-        lat = res.coord.lat;
-        lon = res.coord.lon;
+            lat = res.coord.lat;
+            lon = res.coord.lon;
 
-        //using onecall api for uv index
+            //using onecall api for uv index
 
-        var oneCallQueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=imperial&appid="+ APIKey;
-        
-        $.ajax({
-            url: oneCallQueryURL,
-            method: "GET"
-        })
-        .then(function(oneCallRes) {
-            console.log(oneCallRes);
+            var oneCallQueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=" + APIKey;
 
-            cityUV.text(oneCallRes.current.uvi);
+            $.ajax({
+                url: oneCallQueryURL,
+                method: "GET"
+            })
+                .then(function (oneCallRes) {
+                    console.log(oneCallRes);
 
-            //render cards here
+                    cityUV.text(oneCallRes.current.uvi);
 
-            for (let i = 0; i < 5; i++) {
-                var dailyForecast = oneCallRes.daily[i];
+                    //render cards here
 
-                var cardDiv = $('<div class="card text-white bg-primary mb-3" style="max-width: 18rem;">');
-                
-                var cardBody = $('<div class="card-body">');
 
-                var cardDate = $('<p class="card-text">');
+                    for (let i = 0; i < 5; i++) {
+                        var dailyForecast = oneCallRes.daily[i];
 
-                var cardIcon = $('<img>').attr("src", "http://openweathermap.org/img/w/" + dailyForecast.weather[0].icon + ".png");
+                        var timestamp = oneCallRes.daily[i].dt;
 
-                var cardTemp = $('<p class="card-text">').text("Temperature:" + dailyForecast.temp.day);
+                        var milliseconds = timestamp * 1000 // 1575909015000
 
-                var cardHum = $('<p class="card-text">').text("Humidity:" + dailyForecast.humidity);
+                        var dateObject = new Date(milliseconds)
 
-                cardBody.append(cardDate, cardIcon, cardTemp, cardHum);
-                cardDiv.append(cardBody);
+                        var realDate = dateObject.toLocaleString("en-US", {month: "long", day: "numeric"})
 
-                fiveDayCards.append(cardDiv);
-            }      
+                        var cardDiv = $('<div class="card text-white bg-primary mb-3" style="max-width: 18rem;">');
 
-        })
-    
-    });
+                        var cardBody = $('<div class="card-body">');
+
+                        var cardDate = $('<p class="card-text">').text(realDate);
+
+                        var cardIcon = $('<img>').attr("src", "http://openweathermap.org/img/w/" + dailyForecast.weather[0].icon + ".png");
+
+                        var cardTemp = $('<p class="card-text">').text("Temperature:" + dailyForecast.temp.day);
+
+                        var cardHum = $('<p class="card-text">').text("Humidity:" + dailyForecast.humidity);
+
+                        cardBody.append(cardDate, cardIcon, cardTemp, cardHum);
+                        cardDiv.append(cardBody);
+
+                        fiveDayCards.append(cardDiv);
+                    }
+
+                })
+
+        });
 
 }
 
-$("#searchButton").on("click", function(event) {
+$("#searchButton").on("click", function (event) {
     event.preventDefault();
 
     var cityResult = searchCity.val().trim();
@@ -83,8 +92,8 @@ $("#searchButton").on("click", function(event) {
 })
 
 
-function clear() {
-    $("#searchCity").empty();
-}
+// function clear() {
+//     $("#searchCity").empty();
+// }
 
-$("#clearButton").on("click", clear);
+// $("#clearButton").on("click", clear);
